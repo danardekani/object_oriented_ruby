5d32a1259881bca23ada3600a645b7f91fd7ba84
Daniel <ardekanicapital@gmail.com>
2019-06-17 22:25:19 -0400

    create store items including: color, price, type

---
+++ store_item.rb
@@ -0,0 +1,23 @@
+apples = {
+  color: "red",
+  price: 2,
+  type: "gala"
+}
+
+p apples
+
+oranges = {
+  color: "orange",
+  price: 1.5,
+  type: "Persian"
+}
+
+p oranges
+
+blackberries = {
+  color: "dark purple",
+  price: 5,
+  type: "California"
+}
+
+p blackberries
